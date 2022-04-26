2f3590a060ed7a7f1a785e2b8289dd5cba139824
nick <nicholas.aloi@stonybrook.edu>
2022-04-25 20:47:45 -0400

    hit sound

--- build/Wolfie2D/src/Project/Scenes/GameLevel.ts
+++ build/Wolfie2D/src/Project/Scenes/GameLevel.ts
@@ -178,29 +178,36 @@ export default class GameLevel extends Scene {
                         if(p2.inRange(dmgInfo.get("center"),dmgInfo.get("range"),dmgInfo.get("state"),dmgInfo.get("dir"))){ //if p2 is in range of attack, 
                             if(dmgInfo.get("type") === "s" && !(this.p2action === "blocking")){ // p2 not blocking (p2 attacked)
                                 this.incPlayerLife(Project_Color.BLUE,dmgInfo.get("dmg"));
+                                this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "hit", loop: false, holdReference: false});
                                 p2.changeState(dmgInfo.get("state"));
                             }
                             if(dmgInfo.get("type") === "s" && this.p2action === "blocking"){ // p2 blocking (p1 attacked)
                                 this.incPlayerLife(Project_Color.RED,dmgInfo.get("dmg"));
+                                this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "hit", loop: false, holdReference: false});
                                 p1.changeState(dmgInfo.get("state"));
                             }
                             if(dmgInfo.get("type") === "p" && !(this.p2action === "attacking")){ // p1 grabs, p2 not attacking, p2 takes damage
                                 this.incPlayerLife(Project_Color.BLUE,dmgInfo.get("dmg"));
+                                this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "hit", loop: false, holdReference: false});
                                 p2.changeState(dmgInfo.get("state"));
                             }
                         }
+                        
                     } else {    //p2 attacking
                         if(p1.inRange(dmgInfo.get("center"),dmgInfo.get("range"),dmgInfo.get("state"),dmgInfo.get("dir"))){
                             if(dmgInfo.get("type") === "s" && !(this.p1action === "blocking")){ //p1 not blocking (p1 attacked)
                                 this.incPlayerLife(Project_Color.RED,dmgInfo.get("dmg"));
+                                this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "hit", loop: false, holdReference: false});
                                 p1.changeState(dmgInfo.get("state"));
                             }
                             if(dmgInfo.get("type") === "s" && this.p1action === "blocking"){    //p1 blocking (p2 attacked)
                                 this.incPlayerLife(Project_Color.BLUE,dmgInfo.get("dmg"));
+                                this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "hit", loop: false, holdReference: false});
                                 p2.changeState(dmgInfo.get("state"));
                             }
                             if(dmgInfo.get("type") === "p" && !(this.p1action === "attacking")){ // p2 grabs, p1 not attacking, p1 takes damage
                                 this.incPlayerLife(Project_Color.RED,dmgInfo.get("dmg"));
+                                this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "hit", loop: false, holdReference: false});
                                 p1.changeState(dmgInfo.get("state"));
                             }
                         }
@@ -500,17 +507,15 @@ export default class GameLevel extends Scene {
             if(!prop.visible) continue;
 
             let propAI = prop.ai as Prop;
-            if(this.player1.collisionShape.overlaps(prop.collisionShape) && p1.party != propAI.party && !(this.p1action === "blocking")) {                                                                                                // todo: make projectiles put hit player in hurt stun
-                if(p1.hitWithProp(propAI.buff,propAI.dir.x)) {
-                    this.incPlayerLife(p1.party,propAI.dmg);
-                    prop.visible = false;
-                }
+            if(this.player1.collisionShape.overlaps(prop.collisionShape) && p1.party != propAI.party && !(this.p1action === "blocking")) { // this assumes projectile type is s, change later
+                //p1.hitWithProp(propAI.buff);                                                                                                // todo: make projectiles put hit player in hurt stun
+                this.incPlayerLife(p1.party,propAI.dmg);
+                prop.visible = false;
             }
             if(this.player2.collisionShape.overlaps(prop.collisionShape) && p2.party != propAI.party && !(this.p2action === "blocking")) {
-                if(p2.hitWithProp(propAI.buff,propAI.dir.x)) {
-                    this.incPlayerLife(p2.party,propAI.dmg);
-                    prop.visible = false;
-                }
+                //p2.hitWithProp(propAI.buff);
+                this.incPlayerLife(p2.party,propAI.dmg);
+                prop.visible = false;
             }
 
             this.handleScreenDespawn(prop,viewPort,viewPortSize);
