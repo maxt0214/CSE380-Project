e6839e7a8348d8d03cb2cb4d03a418fb531debeb
maxt0214 <mxt6232996077@gmail.com>
2022-05-09 12:23:09 -0400

    Added end scene

--- build/Wolfie2D/src/Project/Scenes/EndLevel.ts
+++ build/Wolfie2D/src/Project/Scenes/EndLevel.ts
@@ -1,5 +1,6 @@
 import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
 import { GameEventType } from "../../Wolfie2D/Events/GameEventType";
+import Sprite from "../../Wolfie2D/Nodes/Sprites/Sprite";
 import Button from "../../Wolfie2D/Nodes/UIElements/Button";
 import Label from "../../Wolfie2D/Nodes/UIElements/Label";
 import { UIElementType } from "../../Wolfie2D/Nodes/UIElements/UIElementTypes";
@@ -7,11 +8,24 @@ import Color from "../../Wolfie2D/Utils/Color";
 import GameLevel from "./GameLevel";
 import HomeScreen from "./HomeScreen";
 
+enum LevelName {
+    MEADOW = "meadow",
+    BEACH = "beach",
+    MOUNTAIN = "mountain",
+    CITY = "city",
+    UNDERSEA = "undersea",
+    VOLCANO = "volcano"
+}
+
 //EndLevel to only play end music. Then transit back to Homescreen
 export default class EndLevel extends GameLevel {
+    timer: number = 5;
+    curr_scene_cut: number = 0;
+    scenecuts: Sprite[];
 
     loadScene(): void {
         this.load.audio("level_music", "project_assets/music/end.mp3");
+        this.initscenecut(this.initOptions.p1);
     }
 
     unloadScene(){
@@ -20,8 +34,17 @@ export default class EndLevel extends GameLevel {
     }
 
     startScene(): void {
-        super.initLayers();
-        super.initViewport();
+        this.addUILayer("UI");
+        this.addLayer("background", 0);
+        this.viewport.setBounds(0, 0, 640, 640);
+        //init scenecuts
+        this.addscenecut(LevelName.MEADOW);
+        this.addscenecut(LevelName.BEACH);
+        this.addscenecut(LevelName.MOUNTAIN);
+        this.addscenecut(LevelName.CITY);
+        this.addscenecut(LevelName.UNDERSEA);
+        this.addscenecut(LevelName.VOLCANO);
+        this.changescenecut();
         //Center view port
         let size = this.viewport.getHalfSize();
         this.viewport.setFocus(size);
@@ -43,14 +66,64 @@ export default class EndLevel extends GameLevel {
         backBtn.borderRadius = 0;
         backBtn.setPadding(new Vec2(80, 30));
         backBtn.font = "PixelSimple";
-
+    
         // When the skip button is clicked, back to home screen
         backBtn.onClick = () => {
             this.sceneManager.changeToScene(HomeScreen, {}, {});
         }
+
         this.emitter.fireEvent(GameEventType.PLAY_SOUND, {key: "level_music", loop: true, holdReference: true});
     }
 
     updateScene(deltaT: number): void {
+        if(this.timer <= 0) {
+            this.curr_scene_cut = this.curr_scene_cut >= 5 ? 0 : this.curr_scene_cut+1;
+            this.changescenecut();
+        }
+
+        this.timer -= deltaT;
+    }
+
+    addscenecut(scene: LevelName) {
+        var bg = this.add.sprite(scene, "background");
+        bg.scale.set(2, 2);
+		bg.position.copy(this.viewport.getCenter());
+        bg.visible = false;
+        this.scenecuts.push(bg);
+    }
+
+    changescenecut() {
+        for(let i = 0; i < 6; i++) {
+            this.scenecuts[i].visible = (i === this.curr_scene_cut);
+        }
+    }
+
+    initscenecut(character: string) {
+        if(character.includes("fighter")) {
+            this.load.image(LevelName.MEADOW, "project_assets/scenecut/meadow_fighter.png");
+            this.load.image(LevelName.BEACH, "project_assets/scenecut/beach_fighter.png");
+            this.load.image(LevelName.MOUNTAIN, "project_assets/scenecut/mountain_fighter.png");
+            this.load.image(LevelName.CITY, "project_assets/scenecut/city_fighter.png");
+            this.load.image(LevelName.UNDERSEA, "project_assets/scenecut/undersea_fighter.png");
+            this.load.image(LevelName.VOLCANO, "project_assets/scenecut/volcano_fighter.png");
+        }
+
+        if(character.includes("waterlady")) {
+            this.load.image(LevelName.MEADOW, "project_assets/scenecut/meadow_waterlady.png");
+            this.load.image(LevelName.BEACH, "project_assets/scenecut/beach_waterlady.png");
+            this.load.image(LevelName.MOUNTAIN, "project_assets/scenecut/mountain_waterlady.png");
+            this.load.image(LevelName.CITY, "project_assets/scenecut/city_waterlady.png");
+            this.load.image(LevelName.UNDERSEA, "project_assets/scenecut/undersea_waterlady.png");
+            this.load.image(LevelName.VOLCANO, "project_assets/scenecut/volcano_waterlady.png");
+        }
+
+        if(character.includes("dwarf")) {
+            this.load.image(LevelName.MEADOW, "project_assets/scenecut/meadow_dwarf.png");
+            this.load.image(LevelName.BEACH, "project_assets/scenecut/beach_dwarf.png");
+            this.load.image(LevelName.MOUNTAIN, "project_assets/scenecut/mountain_dwarf.png");
+            this.load.image(LevelName.CITY, "project_assets/scenecut/city_dwarf.png");
+            this.load.image(LevelName.UNDERSEA, "project_assets/scenecut/undersea_dwarf.png");
+            this.load.image(LevelName.VOLCANO, "project_assets/scenecut/volcano_dwarf.png");
+        }
     }
 }
