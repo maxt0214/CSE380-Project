8b8bd5d0afaa5e1153db32b4af1abfe9eb1504c0
maxt0214 <mxt6232996077@gmail.com>
2022-05-09 14:44:10 -0400

    Added benchmark4

---
+++ build/benchmark4/project_assets/tilemaps/tiled files/waterlady.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="waterlady" tilewidth="24" tileheight="36" tilecount="54" columns="9">
+ <image source="../../spritesheets/waterlady.png" width="216" height="216"/>
+</tileset>
